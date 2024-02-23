1b31bcef52da5a94614d98ba0ff9dd5d5c1b9c03
Ömür Koca <93597251+jomurKoca@users.noreply.github.com>
2024-02-23 16:39:30 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import java.util.Scanner;
+
+
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int Physik;
+        int Mathe;
+        int Deutsch ;
+        int Chemie;;
+        int Geschichte;
+        int Durchschnitt;
+
+        Scanner display = new Scanner(System.in);
+        System.out.println("Geben Sie Physiksnote ein:");
+        Integer Physiknote = new Integer(display.nextLine());
+
+        Scanner display1 = new Scanner(System.in);
+        System.out.println("Geben Sie Mathenote ein:");
+        Integer Mathenote = new Integer(display1.nextLine());
+
+        Scanner display2 = new Scanner(System.in);
+        System.out.println("Geben Sie Chemienote ein:");
+        Integer Chemienote = new Integer(display2.nextLine());
+
+        System.out.println(" Notendurchschnitt ist : " + (Mathenote+Physiknote+Chemienote)/3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
